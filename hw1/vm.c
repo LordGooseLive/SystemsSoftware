980b7ff4dadb1f61ce1fc7fc69882952a426b496
glitchyxlatrell <latrellkong11@gmail.com>
2026-02-04 01:05:31 +0000

    adding semicolon in SYS

--- hw1/vm.c
+++ hw1/vm.c
@@ -202,7 +202,7 @@ int main(int argc, char *argv[])
                 case 1: //OutputInt
                 {
                     printf("%d\n", pas[sp]);
-                    sp++
+                    sp++;
                     break;
                 }
                 
