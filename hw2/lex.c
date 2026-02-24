40f3db69bcab3a86c0e106305a7acba38450fbec
glitchyxlatrell <latrellkong11@gmail.com>
2026-02-24 20:23:58 +0000

    setting up before logic implementation

--- hw2/lex.c
+++ hw2/lex.c
@@ -17,7 +17,7 @@ where:
 
 Notes:
   - Implement a lexical analyser for the PL/0 language.
-  - The program must detect errors such as 
+  - The program must detect errors such as
     - numbers longer than five digits
     - identifiers longer than eleven characters
     - invalid characters.
@@ -34,50 +34,100 @@ Due Date: Monday, February 3, 2026
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+
+typedef enum
+{
+  skipsym = 1,  // Skip / ignore token
+  identsym,     // Identifier
+  numbersym,    // Number
+  beginsym,     // begin
+  endsym,       // end
+  ifsym,        // if
+  fisym,        // fi
+  thensym,      // then
+  whilesym,     // while
+  dosym,        // do
+  odsym,        // od
+  callsym,      // call
+  constsym,     // const
+  varsym,       // var
+  procsym,      // procedure
+  writesym,     // write
+  readsym,      // read
+  elsesym,      // else
+  plussym,      // +
+  minussym,     // -
+  multsym,      // *
+  slashsym,     // /
+  eqsym,        // =
+  neqsym,       // <>
+  lessym,       // <
+  leqsym,       // <=
+  gtrsym,       // >
+  geqsym,       // >=
+  lparentsym,   // (
+  rparentsym,   // )
+  commasym,     // ,
+  semicolonsym, // ;
+  periodsym,    // .
+  becomessym    // :=
+} TokenType;
 
 int main(int argc, char *argv[])
 {
-    printf("argc = %d\n", argc);
 
-    for (int i = 0; i < argc; i++)
+  for (int i = 0; i < argc; i++)
+  {
+    printf("argv[%d] = %s\n", i, argv[i]);
+  }
+  printf("\n");
+
+
+  FILE *file = NULL;
+  if (argc > 1)
+  {
+    // Opening file for input
+    file = fopen(argv[1], "r");
+    if (file == NULL)
     {
-        printf("argv[%d] = %s\n", i, argv[i]);
+      printf("input file could not be opened");
+      return 1;
     }
+  }
 
-    printf("\n");
+  else
+  {
+    printf("No extra arguments provided.\n");
+    printf("Try: ./00_args 123\n");
+    return 1;
+  }
 
-    if (argc > 1)
-    {
-        int x = atoi(argv[1]);   // convert string to int (simple)
-        printf("Converted argv[1] to int: %d\n", x);
-    }
-    else
-    {
-        printf("No extra arguments provided.\n");
-        printf("Try: ./00_args 123\n");
-        return 1; //error 1: insufficient arguments
-    }
+  // Variable declaration and Initialisation
+  char lexemes[50][12];   // array of lexemes
+  int tokens[50];         // array of tokens
+  int len = 0;            // number of lexemes/ tokens scanned
+  int character = 0;      // used to parse file 
 
-    //Variable declaration and Initialisation
-    char lexemes [50][11] = ""; //array of lexemes
-    int tokens [50] = 0; //array of tokens
-    int len = 0; //number of lexemes/ tokens scanned
+  while((character = fgetc(file)) != EOF )
+  {
+    
+  }
 
-
-    return 0;
+  return 0;
 }
 
-void print (int tokens [], char lexemes[][], int len)
+void print(int tokens[], char lexemes[][12], int len)
 {
-    //print input file  
-    printf("Source Program: \n");
-    //implement logic
+  // print input file
+  printf("Source Program: \n");
+  // implement logic
 
-    //print lexeme-token pairs
-    printf("Lexeme Table:\n");
-    printf("lexeme %t token type");
-    for (int i = 0; i < len; i++)
-    {
-        printf("%s %t %d \n", lexemes[i], tokens[i]);
-    }
+  // print lexeme-token pairs
+  printf("Lexeme Table:\n");
+  printf("lexeme %t token type");
+  for (int i = 0; i < len; i++)
+  {
+    printf("%s %t %d \n", lexemes[i], tokens[i]);
+  }
 }
