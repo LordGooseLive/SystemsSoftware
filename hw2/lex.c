2e9746ba1f5889960154f643c04802e476edb99e
Arav <aravtulsi@gmail.com>
2026-02-25 22:18:08 -0500

    standardised comments and prints, minor bug squashed

--- hw2/lex.c
+++ hw2/lex.c
@@ -31,7 +31,7 @@ Instructor: Dr. Jie Lin
 Due Date: Monday, February 3, 2026
 */
 
-//includes, macros and enums
+// includes, macros and enums
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -43,81 +43,81 @@ Due Date: Monday, February 3, 2026
 
 typedef enum
 {
-  skipsym = 1,  // Skip / ignore token
-  identsym,     // Identifier
-  numbersym,    // Number
-  beginsym,     // begin
-  endsym,       // end
-  ifsym,        // if
-  fisym,        // fi
-  thensym,      // then
-  whilesym,     // while
-  dosym,        // do
-  odsym,        // od
-  callsym,      // call
-  constsym,     // const
-  varsym,       // var
-  procsym,      // procedure
-  writesym,     // write
-  readsym,      // read
-  elsesym,      // else
-  plussym,      // +
-  minussym,     // -
-  multsym,      // *
-  slashsym,     // /
-  eqsym,        // =
-  neqsym,       // <>
-  lessym,       // <
-  leqsym,       // <=
-  gtrsym,       // >
-  geqsym,       // >=
-  lparentsym,   // (
-  rparentsym,   // )
-  commasym,     // ,
-  semicolonsym, // ;
-  periodsym,    // .
-  becomessym    // :=
+    skipsym = 1,  // Skip / ignore token
+    identsym,     // Identifier
+    numbersym,    // Number
+    beginsym,     // begin
+    endsym,       // end
+    ifsym,        // if
+    fisym,        // fi
+    thensym,      // then
+    whilesym,     // while
+    dosym,        // do
+    odsym,        // od
+    callsym,      // call
+    constsym,     // const
+    varsym,       // var
+    procsym,      // procedure
+    writesym,     // write
+    readsym,      // read
+    elsesym,      // else
+    plussym,      // +
+    minussym,     // -
+    multsym,      // *
+    slashsym,     // /
+    eqsym,        // =
+    neqsym,       // <>
+    lessym,       // <
+    leqsym,       // <=
+    gtrsym,       // >
+    geqsym,       // >=
+    lparentsym,   // (
+    rparentsym,   // )
+    commasym,     // ,
+    semicolonsym, // ;
+    periodsym,    // .
+    becomessym    // :=
 } TokenType;
 
-//function prototypes
-int streq (char stringA [], char stringB []); //String equal? 1 : 0
-int nameExists (char name [], char names[][11], int num_names); //checks if the name is already in name table
+// Function prototypes
+int streq (char stringA [], char stringB []); // String equal? 1 : 0
+int nameExists (char name [], char names[][11], int num_names); // Name present? 1 : 0
 
-//--- programme body ---
+//--- Programme Body ---
 
 int main(int argc, char *argv[])
 {
     // Variable declaration and Initialisation
-    char lexemes[50][12];   // array of lexemes
-    char names [50][11];    // array of names
-    int tokens[50];         // array of tokens
-    int num_lex = 0;        // number of lexemes/ tokens scanned
-    int character = 0;      // used to parse file 
-    int num_names = 0;      // number of names scanned
-    FILE *file = NULL;      // stores input file
+    char lexemes[50][12];   // Array of lexemes
+    char names [50][11];    // Array of names
+    int tokens[50];         // Array of tokens
+    int num_lex = 0;        // Number of lexemes/ tokens scanned
+    int character = 0;      // Used to parse file 
+    int num_names = 0;      // Number of names scanned
+    FILE *file = NULL;      // Stores input file
 
-    //--- validate inputs ---
+    // --- Validate inputs ---
 
-    //print all inputs
+    // Print all inputs
     for (int i = 0; i < argc; i++)
     {
         printf("argv[%d] = %s\n", i, argv[i]);
     }
     printf("\n");
 
-    //ensure only one input is given and it can be accessed
+    // Ensure only one input is given and it can be accessed
     if (argc == 2)
     {
         // Opening file for input
         file = fopen(argv[1], "r");
         if (file == NULL)
         {
-            printf("input file could not be opened");
+            printf("Input file could not be opened\n");
             return 1;
         }
     }
 
-    // print error message if invalid number of arguments
+    // Print error message if invalid number of arguments
     else
     {
         printf("No extra arguments provided.\n");
@@ -125,47 +125,46 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    //--- parsing, character by character, until end of file---
+    //--- Parsing, character by character, until end of file---
 
-    /*  First checks if char that we collect is a comment delimiter
-        or invisible character. skips comments and invisible characters.
-        Next, collects lexemes and attempts to match with token type
-        using enum TokenType. 
-        If lexeme is alphanumeric but not a reserve word, it is a name. 
-        Stores name to name table if it is new.
-        If lexeme is a number, ensures it is valid then stores.
-        if lexeme is a special symbol, stores.  
+    /*   - First checks if char is a comment delimiter or invisible character. 
+            - skips comments and invisible characters.
+        - Next, collects lexemes and attempts to match with token type 
+            - If lexeme is alphanumeric but not a reserve word, it is a name. 
+            - Stores name to name table if it is new.
+            - If lexeme is a number, ensures it is valid then stores.
+            - If lexeme is a special symbol, stores.  
     */
     while((character = fgetc(file)) != EOF )
     {
-        // skipping if encountering invisible characters
+        // Skipping if encountering invisible characters
         if(isspace(character))
         {
             continue;
         }
 
-        // skipping if encountering multi-line comments
+        // Skipping if encountering multi-line comments
         else if(character == '/')
         {
             int temp  = fgetc(file);
-            int breakLoop = 1; //flag
+            int breakLoop = 1; // Flag
 
             if(temp == '*')
             {
-                // parsing through whole comment
+                // Parsing through whole comment
                 while(breakLoop == 1)
                 {
                     character = fgetc(file);
 
-                    // error catching if comment goes until EOF
+                    // Error catching if comment goes until EOF
                     if(character == EOF)
                     {
-                        printf("error with multi-line comment");
+                        printf("Error with multi-line comment");
                         breakLoop = 0;
                         continue;
                     }
                         
-                    // checking as might be end of multi-line comment
+                    // Checking as might be end of multi-line comment
                     else if(character == '*')
                     {
                         temp = fgetc(file);
@@ -181,13 +180,13 @@ int main(int argc, char *argv[])
                         }
                     }
 
-                    // continuing to next character in comment
+                    // Continuing to next character in comment
                     continue;
                 }
                 continue;   
             }
 
-            // going back if not multi-line comment and just a slash
+            // Going back if not multi-line comment and just a slash
             else
             {
                 ungetc(temp, file);
@@ -197,39 +196,39 @@ int main(int argc, char *argv[])
             }
         }
 
-        // if character is a letter(identifier)
+        // If character is a letter(identifier)
         else if(isalpha(character))
         {
-            // initializing first letter of identifier and counter
+            // Initializing first letter of identifier and counter
             int counter = 0;
             lexemes[num_lex][counter++] = character;
 
-            // getting next character and checking if it is letter/number
+            // Getting next character and checking if it is letter/number
             int temp = fgetc(file);
             while(temp != EOF && isalnum(temp))
             {
                 // checking length
                 if(counter >= 11)
                 {
-                    printf("Identifier is too long");
+                    printf("Identifier is too long\n");
                     break;
                 }
 
-                // inputting character into string and getting next character
+                // Inputting character into string and getting next character
                 lexemes[num_lex][counter++] = temp;
                 temp = fgetc(file);
             }
             
-            // adding null terminator to end
+            // Adding null char (terminator) to end
             lexemes[num_lex][counter] = '\0';
 
-            // if was not letter/number, ungetting character
+            // If was not letter/number, ungetting character
             if(temp != EOF)
             {
                 ungetc(temp, file);
             }
 
-            // checking if identifier is a reserved word
+            // Checking if lexeme is a reserved word
             if(streq(lexemes[num_lex], "begin"))
             {
                 tokens[num_lex] = beginsym;
@@ -306,10 +305,10 @@ int main(int argc, char *argv[])
                 tokens[num_lex] = elsesym;
             }
 
-            else
+            else // Not reserved word, so is an identifier
             {
                 tokens[num_lex] = identsym;
-                // if identifier is not already in name table, add it
+                // If identifier is not already in name table, add it
                 if(!nameExists(lexemes[num_lex], names, num_names))
                 {
                     strcpy(names[num_names], lexemes[num_lex]);
@@ -317,100 +316,108 @@ int main(int argc, char *argv[])
                 }
             }
 
-            // incrementing length of lexemes and tokens list
+            // Incrementing length of lexemes and tokens list
             num_lex++;
         }
         
-        // if character is a number
+        // If character is a number
         else if(isdigit(character))
         {
-            // initializing counter and first character of number
+            // Initializing counter and first character of number
             int counter = 0;
             lexemes[num_lex][counter++] = character;
 
-            // getting next character and checking if it is a number
+            // Getting next character and checking if it is a number
             int temp = fgetc(file);
 
             while(temp != EOF && isdigit(temp))
             {
-                // checking if number is too long
+                // Checking if number is too long
                 if(counter >= 5)
                 {
-                    printf("Number is too long");
+                    printf("Number is too long\n");
                     break;
                 }
 
-                // putting next character into lexeme
+                // Putting next character into lexeme
                 lexemes[num_lex][counter++] = temp;
                 temp = fgetc(file);
             }
 
-            // error if letter is next to digit
+            // Error if letter is next to digit
             if(isalpha(temp))
             {
-                printf("invalid number (has letters)");
+                printf("Invalid number (has letters)\n");
             }
 
-            // ungetting temp if it was another symbol after number
+            // Ungetting temp if it was another symbol after number
             if(temp != EOF)
             {
                 ungetc(temp, file);
             }
             
-            // adding null terminator to number and putting token in
+            // Adding null terminator to number and putting token in
             lexemes[num_lex][counter] = '\0';
             tokens[num_lex++] = numbersym;
         }
 
-        // checking for special symbols
+        // Checking for special symbols
         else if(character == '+')
         {
             lexemes[num_lex][0] = character;
             lexemes[num_lex][1] = '\0';
             tokens[num_lex++] = plussym;
         }
+
         else if(character == '-')
         {
             lexemes[num_lex][0] = character;
             lexemes[num_lex][1] = '\0';
             tokens[num_lex++] = minussym;
         }
+
         else if(character == '*')
         {
             lexemes[num_lex][0] = character;
             lexemes[num_lex][1] = '\0';
             tokens[num_lex++] = multsym;
         }
+
         else if(character == '=')
         {
             lexemes[num_lex][0] = character;
             lexemes[num_lex][1] = '\0';
             tokens[num_lex++] = eqsym;
         }
+
         else if(character == '(')
         {
             lexemes[num_lex][0] = character;
             lexemes[num_lex][1] = '\0';
             tokens[num_lex++] = lparentsym;
         }
+
         else if(character == ')')
         {
             lexemes[num_lex][0] = character;
             lexemes[num_lex][1] = '\0';
             tokens[num_lex++] = rparentsym;
         }
+
         else if(character == ',')
         {
             lexemes[num_lex][0] = character;
             lexemes[num_lex][1] = '\0';
             tokens[num_lex++] = commasym;
         }
+
         else if(character == ';')
         {
             lexemes[num_lex][0] = character;
             lexemes[num_lex][1] = '\0';
             tokens[num_lex++] = semicolonsym;
         }
+
         else if(character == '.')
         {
             lexemes[num_lex][0] = character;
@@ -418,7 +425,7 @@ int main(int argc, char *argv[])
             tokens[num_lex++] = periodsym;
         }
 
-        // if encounters '<', using if and else statement to determine which special symbol
+        // If encounters '<', using if and else statement to determine which special symbol
         else if(character == '<')
         {
             int temp = fgetc(file);
@@ -448,7 +455,7 @@ int main(int argc, char *argv[])
             }
         }
 
-        // if encounters '>', using if and else statement to determine which special symbol
+        // If encounters '>', using if and else statement to determine which special symbol
         else if(character == '>')
         {
             int temp = fgetc(file);
@@ -470,7 +477,7 @@ int main(int argc, char *argv[])
             }
         }
         
-        // if encounters ':', using if and else statement to determine if it is becomessym
+        // If encounters ':', using if and else statement to determine if it is becomessym
         else if(character == ':')
         {
             int temp = fgetc(file);
@@ -485,69 +492,69 @@ int main(int argc, char *argv[])
 
             else 
             {
-                printf("invalid symbol (no token representation)");
-                ungetc(file, temp);
+                printf("Invalid symbol (no token representation)\n");
+                ungetc(temp, file);
             }
         }
 
-        // else statement to catch any outliers
+        // Else statement to catch any outliers
         else
         {
             printf("Invalid character scanned");
         }
     }
 
-    //--- Print all data ---
+    // --- Print all data ---
 
-    //Lexeme Table
-    printf("Lexeme Table:\n");
-    printf("\nlexeme \t token type \n");
-
-    for (int i = 0; i < num_lex; i++)
+    // Lexeme Table
+    printf("Lexeme Table:\n"); // Header
+    printf("\nLexeme \t Token Type \n"); // Column titles
+    for (int i = 0; i < num_lex; i++) // Iterating through all lexemes and tokens
     {
         printf("%s \t %d \n", lexemes[i], tokens[i]);
     }
 
-    //Name Table
-    printf("\nName Table:\n");
-    printf("\nIndex \t Name\n");
-    for (int i = 0; i < num_names; i++)
+    // Name Table
+    printf("\nName Table:\n"); // Header
+    printf("\nIndex \t Name\n"); // Column titles
+    for (int i = 0; i < num_names; i++) // Iterating through all names
     {
         printf("%d \t %s \n", i, names[i]);
     }
 
-    //Token list
-    printf("\n Token List:\n\n");
-    for (int i = 0; i < num_lex; i++)
+    // Token list
+    printf("\n Token List:\n\n"); // Header
+    for (int i = 0; i < num_lex; i++) // Iterating through all tokens
     {
-        printf("%s", tokens[i]);
+        printf("%s ", tokens[i]);
     }
 
-    return 0; //everything went well
+    return 0; // Everything went well
 }
 
-// ---helper function definitions ---
+// ---Helper function definitions ---
 
-//function to check if two strings are identical. returns true or false
-int streq (char stringA [], char stringB []) //String equal? 1 : 0
+// Checks if two strings are identical. Returns true or false
+int streq (char stringA [], char stringB []) // String equal? 1 : 0
 {
     if (strcmp(stringA, stringB) == 0)
         return 1; //true
+
     else
         return 0; //false
 }
 
-//checks if the name is already in name table. returns true or false
-int nameExists (char name [], char names[][11], int num_names) //name present? 1 : 0
+// Checks if the name is already in name table.  Returns true or false
+int nameExists (char name [], char names[][11], int num_names) // Name present? 1 : 0
 {
-    int retval = 0; //false by default
+    int retval = 0; // False by default
 
     for (int i = 0; i < num_names; i++)
     {
         if (streq (name, names[i]))
         {   
-            retval = 1; //sets true since name is present
-            break; //no need to continue since we found the name
+            retval = 1; // Sets true since name is present
+            break; // No need to continue since we found the name
         }
     }
     return retval;
